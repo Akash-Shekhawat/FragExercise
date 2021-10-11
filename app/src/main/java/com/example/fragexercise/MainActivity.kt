10ac6276c7a7f2730fb6511be82827edcefaba4b
Akash-Shekhawat <akash.shekhawat@tothenew.com>
2021-10-11 22:22:48 +0530

    Commit1

--- app/src/main/java/com/example/fragexercise/MainActivity.kt
+++ app/src/main/java/com/example/fragexercise/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.fragexercise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Get views reference
+        val llbtn = findViewById<Button>(R.id.linearLayoutDemoBtn)
+        val rlBtn = findViewById<Button>(R.id.relativeLayoutDemoBtn)
+        val clBtn = findViewById<Button>(R.id.constraintLayoutDemoBtn)
+
+        // open Linear layout demo activity
+        llbtn.setOnClickListener {
+            startActivity(Intent(this, LinearLayout::class.java))
+        }
+
+        // open Relative layout demo activity
+        rlBtn.setOnClickListener {
+            startActivity(Intent(this, Relative::class.java))
+        }
+
+        // open Linear layout demo activity
+        clBtn.setOnClickListener {
+            startActivity(Intent(this, ConstraintLayout::class.java))
+        }
     }
 }
